33531e23ddcbf6d4877f8ed6d1ec80b8023858da
tylzh97 <tylzh97@126.com>
2018-09-04 13:05:38 +0800

    Initial Commit

---
+++ LoversUniversity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoversUniversity
+//
+//  Created by 李政浩 on 2018/9/4.
+//  Copyright © 2018年 李政浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LoversUniversity/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoversUniversity
+//
+//  Created by 李政浩 on 2018/9/4.
+//  Copyright © 2018年 李政浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
